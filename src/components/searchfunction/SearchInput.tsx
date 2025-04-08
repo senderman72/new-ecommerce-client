bdc0e573a25f5bd2fff5c636862514a69cf6f2d4
Erik Persson <perssonerik7@gmail.com>
2025-04-08 16:41:23 +0200

    integrerade sök apiet

--- src/components/searchfunction/SearchInput.tsx
+++ src/components/searchfunction/SearchInput.tsx
@@ -3,12 +3,35 @@ import {
   StyledSearchForm,
   StyledSearchInput,
 } from "../styled/styledsearch/StyledSearchinput";
+import { ChangeEvent, FormEvent, use, useState } from "react";
+import { getGoogleSearch } from "../../services/googleApi/getGoogleSearch";
 
 const SearchInput = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleSearch = async (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!searchQuery) return;
+
+    try {
+      const response = await getGoogleSearch(searchQuery);
+      console.log(response);
+    } catch {
+      console.log("something went wrong");
+    }
+  };
+
   return (
     <StyledSearchForm>
-      <StyledSearchInput type="text" />
-      <IoMdSearch size={50} />
+      <StyledSearchInput
+        type="text"
+        placeholder="sök"
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          setSearchQuery(e.target.value);
+        }}
+      />
+      <IoMdSearch size={50} onClick={handleSearch} />
     </StyledSearchForm>
   );
 };
--- src/services/googleApi/getGoogleSearch.ts
+++ src/services/googleApi/getGoogleSearch.ts
@@ -1,9 +1,15 @@
 export const getGoogleSearch = async (query: string) => {
+  const key = import.meta.env.VITE_GOOGLE_KEY;
+  const cx = import.meta.env.VITE_SEARCH_ENGINE_ID;
+
   try {
-    // const response = await fetch(
-    //   `https://www.googleapis.com/customsearch/v1?key=${process.env.REACT_APP_GOOGLE_API_KEY}&cx=${process.env.REACT_APP_GOOGLE_SEARCH_ENGINE_ID}&q=${query}`
-    // );
+    const response = await fetch(
+      `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(
+        query
+      )}&key=${key}&cx=${cx}`
+    );
     const data = await response.json();
+    console.log(data);
     return data;
   } catch {
     console.log("Kunde inte hitta data");
