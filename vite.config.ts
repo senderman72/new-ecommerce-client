b22e712c2c9de0f05323ed61beb43556c3bb0e8c
Erik Persson <perssonerik7@gmail.com>
2025-04-08 14:26:14 +0200

    update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/", // Ensure correct asset loading in production
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000 KB (1MB)
+  },
 });
